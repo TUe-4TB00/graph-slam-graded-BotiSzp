517df78adf641ec119361ca07a2e2f10d1d2b43e
Botond Szép <b.z.szep@student.tue.nl>
2026-05-28 15:19:08 +0200

    optimization graph 8.1 + add landmark 8.2

--- src/add_landmark_measurement.py
+++ src/add_landmark_measurement.py
@@ -8,8 +8,14 @@ ODOMETRY_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.2, 0.2, 0.1]))  # 
 MEASUREMENT_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.05, 0.1]))  # (bearing, range)
 
 def add_landmark_measurement(graph, initial_estimate, result):
-    # Determine the correct rotation (bearing) and distance from X(4) to L(2) 
-    # rotation = 
-    # distance = 
-    graph.add(gtsam.BearingRangeFactor2D(X(4), L(2), gtsam.Rot2.fromDegrees(rotation), distance, MEASUREMENT_NOISE))
+    #optimized results extected
+    pose_4 = result.atPose2(X(4))
+    landmark_2 = result.atPoint2(L(2))
+    
+    #gstam
+    distance = pose_4.range(landmark_2)
+    bearing = pose_4.bearing(landmark_2) 
+    #addd measurement to graph and change it
+    graph.add(gtsam.BearingRangeFactor2D(X(4), L(2), bearing, distance, MEASUREMENT_NOISE))
+    
     return graph
